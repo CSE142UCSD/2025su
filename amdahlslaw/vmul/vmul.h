4ea35972e6a70e65b592b187431e61389c068519
hunweitseng <h1tseng@cs.ucsd.edu>
2025-08-06 13:11:19 -0700

    Metrics

---
+++ amdahlslaw/vmul/vmul.h
@@ -0,0 +1,12 @@
+#ifndef NUM_OF_THREADS
+#define NUM_OF_THREADS 0
+#define CPU_TIME 1
+#define HOST2GPU 2
+#define GPU_KERNEL 3
+#define GPU2HOST 4
+
+
+
+void vector_mul_double_cuda(double *va, double *vb, unsigned long int size, int p, int iter);
+void vector_mul_double(double *va, double *vb, unsigned long int size, int p, int iter);
+#endif
