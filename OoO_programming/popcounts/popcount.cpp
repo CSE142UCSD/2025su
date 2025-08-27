42166d0bdd4e8a2f0c768fffce0147e5fc75391f
hunweitseng <h1tseng@cs.ucsd.edu>
2025-08-27 15:58:55 -0700

    Updated Demo

--- OoO_programming/popcounts/popcount.cpp
+++ OoO_programming/popcounts/popcount.cpp
@@ -2,6 +2,13 @@
 #include<assert.h>
 #include <stdint.h>
 #include <stdio.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "perfstats.h"
+#ifdef __cplusplus
+}
+#endif
 #ifdef SSE42
 #include <smmintrin.h>
 inline int popcount(uint64_t x) {
@@ -15,8 +22,8 @@ inline int popcount(uint64_t x) {
      int c = 0;
      int table[16] = {0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4};
      while(x) {
-	       c += table[(x & 0xF)];
-	       x = x >> 4;
+            c += table[(x & 0xF)];
+            x = x >> 4;
      }
      return c;
 }
@@ -27,14 +34,14 @@ inline int popcount(uint64_t x) {
     int c = 0;
     while(x) //while any bits are set not sure how you are getting the value
     {
-       c += x & 1;
-       x = x >> 1;
-       c += x & 1;
-       x = x >> 1;
-       c += x & 1;
-       x = x >> 1;
-       c += x & 1;
-       x = x >> 1;
+        c += x & 1;
+        x = x >> 1;
+        c += x & 1;
+        x = x >> 1;
+        c += x & 1;
+        x = x >> 1;
+        c += x & 1;
+        x = x >> 1;
     }
     return c;
 }
@@ -44,8 +51,8 @@ inline int popcount(uint64_t x) {
      int c = 0;
      int table[16] = {0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4};
      for (uint64_t i = 0; i < 16; i++) {
-	       c += table[(x & 0xF)];
-	       x = x >> 4;
+            c += table[(x & 0xF)];
+            x = x >> 4;
      }
      return c;
 }
@@ -79,8 +86,8 @@ inline int popcount(uint64_t x) {
 }
 #else //A
 inline int popcount(uint64_t x){
-	int c=0;
-	while(x) //while any bits are set not sure how you are getting the value
+    int c=0;
+    while(x) //while any bits are set not sure how you are getting the value
     {
        c += x & 1;
        x = x >> 1;
@@ -107,14 +114,23 @@ int identity(uint64_t x) {
 int main(int argc, char *argv[]) {
 
      uint64_t key = 0xdeadbeef;
-
+     char preamble[1024];
+     char epilogue[1024];
+     char header[1024];
+     char stat_file[] = "stats.csv";
      int count = 1000000000;
      uint64_t sum = 0;
-     
+     perfstats_init();
+     perfstats_enable(1);     
      for (int i=0; i < count; i++)
      { 
-	   sum += popcount (RandLFSR(key)); 
+        sum += popcount (RandLFSR(key)); 
      }
+     perfstats_disable(1);
+     sprintf(epilogue,"\n");
+     sprintf(preamble,"");
+     perfstats_print(preamble, stat_file, epilogue);
+     perfstats_deinit();
      printf("Result: %lu\n", sum);
      return sum;
 }
